9cb843e910b8e8c818c221faab7ca205430a5fcd
Fabricio1805 <fabricio.ss09@outlook.com>
2025-05-26 19:00:45 -0300

    (feature): security in routes Product

--- src/main/java/com/devsuperior/dscommerce/application/controller/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/application/controller/ProductController.java
@@ -6,21 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.DeleteExchange;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscommerce.application.dto.ProductDTO;
 import com.devsuperior.dscommerce.infrastructure.service.ProductService;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -42,6 +42,7 @@ public class ProductController {
         return ResponseEntity.ok().body(productService.findAll(pageable));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> save(@Valid @RequestBody ProductDTO productDTO) {
         ProductDTO result = productService.save(productDTO);
@@ -49,14 +50,15 @@ public class ProductController {
         return ResponseEntity.created(uri).body(result);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable Long id,@Valid @RequestBody ProductDTO entity) {
         productService.update(id, entity);
         return ResponseEntity.noContent().build();
     }
     
-
-    @DeleteExchange("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.delete(id);
         return ResponseEntity.noContent().build();
